573a8b659837cb3372ebaf7f517e53bcde687be2
Yasser Benseghier <93477835+Yasser4Coder@users.noreply.github.com>
2024-12-10 00:49:16 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDatabase from "./config/database.js";
+import registerationRoute from "./routes/registeration.routes.js";
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// Connect to the database
+connectDatabase();
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
+
+app.use("/api/users", registerationRoute);
+
+// Default route for undefined endpoints
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
